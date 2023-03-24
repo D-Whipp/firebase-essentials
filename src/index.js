0d4824b534b273aecbf55b95246b0452edc3dce5
David Whipple <dwhipp88@gmail.com>
2023-03-24 16:36:54 -0600

    installed firebase

--- src/index.js
+++ src/index.js
@@ -6,13 +6,37 @@ import { App, serviceWorker } from './app';
 import 'react-dates/initialize';
 import 'react-dates/lib/css/_datepicker.css';
 
+// FIREBASE
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// import * as firebase from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAGNmkMlTkYzLAh85Z2ORS7iarmE6naH18",
+  authDomain: "restaurant-reservations-83354.firebaseapp.com",
+  projectId: "restaurant-reservations-83354",
+  storageBucket: "restaurant-reservations-83354.appspot.com",
+  messagingSenderId: "775442463428",
+  appId: "1:775442463428:web:ff742f705249bdb96ba791",
+  measurementId: "G-MWSEHX6ETD"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 // This is where the magic happens. React renders our App component
 // inside the div with the id "root"
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
